48a8252ab01fbd8f97fee07eb871396cbbed508d
Damian Cyrana <damiancyrana@gmail.com>
2021-04-27 20:49:42 +0200

    Implement function to chceck users

--- manage_users_account/manage_users_account.sh
+++ manage_users_account/manage_users_account.sh
@@ -1,18 +1,31 @@
 #!/bin/bash"
 
-if [$# -eq 0]; then
-	read -p "Enter username: " user_name
+user_prompt() {
+	message=${1:-"Enter the account details"}
+	echo "$message"
+	read -p "Enter user name: " user_name
 	read -sp "Enter a password: " user_password
 	echo ""
-
 	read -sp "Enter the password again: " user_password_check
-	if ["$user_password" != "$user_password_check"]; then
-		echo -e "\n${0}: Password don't match!"
-		exit 1
-	fi
+	echo ""
+}
+
+user_check() {
+	grep -q \^${1}\: /etc/passwd && return 0	
+}
+
+
+if [ $# -eq 0 ]; then
+	user_prompt
+	while [ "$user_password" != "$user_password_check" ]; do
+		user_prompt "Passwords didn't match!"
+	done
 else
 	user_name="$1"
 	user_password="{2:-Password1}"
 fi
-echo -e "\n$user_name $user_password"
 
+if user_check "$user_name"; then
+	prompt_user "The user name already exist! Please try other creditionals"
+fi
+echo -e "\n$user_name $user_password"
